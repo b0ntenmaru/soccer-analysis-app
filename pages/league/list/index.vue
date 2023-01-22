4b8992228046272baf6f259b7c61bc0cdd3afbbd
Hiroaki Date <mmst3l4@gmail.com>
2023-01-22 21:22:11 +0900

    リーグ一覧画面を作成

---
+++ pages/league/list/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+type UserJson = {
+  id: number;
+  name: string;
+}
+
+const { data: users } = useFetch<Array<UserJson>>('api/users', {
+  server: false,
+});
+</script>
+
+<template>
+  <h1>リーグ一覧画面</h1>
+
+</template>
