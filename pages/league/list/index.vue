e43251a98330f76c8e62fc9e3b612d7624f1d936
Hiroaki Date <mmst3l4@gmail.com>
2023-01-28 02:09:30 +0900

    リーグ一覧からリーグスタッツ詳細画面へ遷移できるようにした

--- pages/league/list/index.vue
+++ pages/league/list/index.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts">
-const { data } = useFetch('/api/league/list');
+const { data: leagueListData } = useFetch('/api/league/list');
 </script>
 
 <template>
   <h1>リーグ一覧画面</h1>
-  {{ data }}
+  <ul>
+    <template v-for="league in leagueListData">
+      <li>
+        {{ league.name }}
+      </li>
+      <li>
+        <ul v-for="s in league.season">
+          <li>
+            <NuxtLink :to="`/league/stats/${s.id}`">
+              {{ s.year }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </li>
+    </template>
+  </ul>
 </template>
