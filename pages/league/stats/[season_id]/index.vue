a0735f055ca90e57acb8253e5299b5267116f3ee
Hiroaki Date <mmst3l4@gmail.com>
2023-01-27 22:30:56 +0900

    リーグスタッツ取得のためのAPIを実装

---
+++ pages/league/stats/[season_id]/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const seasonId = useRoute().params.season_id;
+const { data } = await useFetch(`/api/league-season/${seasonId}`)
+</script>
+
+<template>
+  <h1>リーグスタッツ閲覧画面</h1>
+  {{ data }}
+</template>
---
+++ types/footy_stats_api/LeagueStatsPage.ts
@@ -0,0 +1,23 @@
+/**
+ * リーグスタッツAPIが返却する値
+ */
+type LeagueStatsPage = {
+  season_id: number;
+  name: string;
+  english_name: string;
+  name_jp: string;
+  country: string;
+  status: 'complete' | 'suspended' | 'canceled' | 'incomplete';
+  division: number; // 何部か
+  starting_year: number;
+  ending_year: number;
+  image: string; // リーグのイメージ画像パス
+  club_num: number; // リーグ数
+  season: string; // example: "2016/2017"
+  totalMatches: number;
+  matchesCompleted: number; // 完了した試合数
+  canceledMatchesNum: number; // キャンセルされた試合数
+  game_week: number; // 現在の試合週
+  total_game_week: number; // 総試合週数
+  progress: number; // 試合の進捗（%）
+}
