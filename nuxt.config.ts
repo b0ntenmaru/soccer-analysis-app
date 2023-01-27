a3256695319772e569d9317f5aa29e6473651452
Hiroaki Date <mmst3l4@gmail.com>
2023-01-27 22:04:46 +0900

    リーグ一覧を取得するserver routesを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  runtimeConfig: {
+    public: {
+      API_PATH: process.env.API_PATH,
+      API_KEY: process.env.API_KEY,
+    },
+  }
 })
