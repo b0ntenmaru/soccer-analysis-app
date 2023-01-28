f31aa1a855c261ba2ed6178e621836807dffb746
Hiroaki Date <mmst3l4@gmail.com>
2023-01-28 16:06:19 +0900

    serverとclientにディレクトリ構成を分けた

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src/client',
+  serverDir: 'src/server',
   runtimeConfig: {
     public: {
       API_PATH: process.env.API_PATH,
