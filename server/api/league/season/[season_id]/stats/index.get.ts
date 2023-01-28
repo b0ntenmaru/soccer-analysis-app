0b3273ce7671ff615c417d5da695ec707c7a7eae
Hiroaki Date <mmst3l4@gmail.com>
2023-01-28 15:20:42 +0900

    convertToLeagueStatsを実装

--- server/api/league/season/[season_id]/stats/index.get.ts
+++ server/api/league/season/[season_id]/stats/index.get.ts
@@ -6,5 +6,58 @@ export default defineEventHandler(async (event) => {
   const seasonId = event.context.params.season_id;
 
   const response: any = await $fetch(`${apiPath}/league-season?key=${apiKey}&season_id=${seasonId}`)
-  return response.data;
+  return convertToLeagueStats(response.data);
 })
+
+/**
+ * footystats apiから返却されたデータをフロントエンド用に変換する
+ */
+const convertToLeagueStats = (fsApiResponseData: any): LeagueStatsData => {
+  const {
+    season_id,
+    name,
+    english_name,
+    name_jp,
+    country,
+    status,
+    division,
+    starting_year,
+    ending_year,
+    image,
+    club_num,
+    season,
+    totalMatches,
+    matchesCompleted,
+    canceledMatchesNum,
+    game_week,
+    total_game_week,
+    progress,
+    top_scorers,
+    top_assists,
+    top_clean_sheets,
+  } = fsApiResponseData;
+
+  return {
+    season_id,
+    name,
+    english_name,
+    name_jp,
+    country,
+    status,
+    division,
+    starting_year,
+    ending_year,
+    image,
+    club_num,
+    season,
+    totalMatches,
+    matchesCompleted,
+    canceledMatchesNum,
+    game_week,
+    total_game_week,
+    progress,
+    top_scorers,
+    top_assists,
+    top_clean_sheets,
+  }
+}
