f343b7af3439c4019c678d3198e58e44479634af
Hiroaki Date <mmst3l4@gmail.com>
2023-02-02 00:10:02 +0900

    リーグ取得のためのAPIを実装

---
+++ src/client/pages/leagues/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const leagueId = useRoute().params.id;
+const { data: league } = await useFetch(`/api/v1/leagues/${leagueId}`);
+</script>
+
+<template>
+  <!--
+    リーグ詳細画面
+    リーグプロフィール
+    リーグ一覧
+    得点ランキングなど
+  -->
+
+  {{ league }}
+</template>
