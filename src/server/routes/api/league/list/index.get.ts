4231dcd4e89604efd0f686db4225272d44983303
Hiroaki Date <mmst3l4@gmail.com>
2023-02-01 23:04:39 +0900

    不要なロジックと型定義を削除

--- src/server/routes/api/league/list/index.get.ts
+++
@@ -1,8 +0,0 @@
-const config = useRuntimeConfig()
-const apiPath = config.public.API_PATH;
-const apiKey = config.public.API_KEY;
-
-export default defineEventHandler(async (event): Promise<LeagueListData> => {
-  const response: any = await $fetch(`${apiPath}/league-list?key=${apiKey}`)
-  return response.data;
-})
