8e929c3f9aee50aaa2d57f7a84c8179486d17823
Hiroaki Date <mmst3l4@gmail.com>
2023-02-02 20:02:35 +0900

    リーグ一覧画面に最低限必要な要素を表示

--- src/client/pages/leagues/index.vue
+++ src/client/pages/leagues/index.vue
@@ -3,5 +3,11 @@ const { data: leagues } = await useFetch('/api/v1/leagues');
 </script>
 
 <template>
-  {{ leagues }}
+  <div v-if="leagues" >
+    <ul>
+      <li v-for="league in leagues">
+        <NuxtLink :to="`/leagues/${league.id}`">{{ league.name }}</NuxtLink>
+      </li>
+    </ul>
+  </div>
 </template>
