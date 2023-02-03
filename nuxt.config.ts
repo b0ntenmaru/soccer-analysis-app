0412ee770ef2f5708ecb1485b79fae4ded3e118d
Hiroaki Date <mmst3l4@gmail.com>
2023-02-03 13:56:42 +0900

    nuxtからvuetifyを呼び出せるようにする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,8 @@ export default defineNuxtConfig({
       API_KEY: process.env.API_KEY,
     },
   },
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
 })
