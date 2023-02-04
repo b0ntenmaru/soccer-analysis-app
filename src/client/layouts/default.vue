8b98bb6f29d0fc26252d9539cd5290c2e58c520c
Hiroaki Date <mmst3l4@gmail.com>
2023-02-04 16:33:22 +0900

    vuetifyに依存していたコードを削除

--- src/client/layouts/default.vue
+++ src/client/layouts/default.vue
@@ -1,63 +1,11 @@
 <script setup lang="ts">
-const state = reactive({
-  links: [
-    {
-      icon: 'mdi-folder',
-      title: 'リーグ一覧',
-      path: '/leagues'
-    }
-  ]
-});
 </script>
 
 <template>
-  <v-app>
-    <v-layout>
-      <v-navigation-drawer
-        color="rgb(31 41 55 / 1)"
-        theme="dark"
-        style="position: fixed"
-      >
-        <v-list>
-          <v-list-item
-            prepend-avatar="https://pbs.twimg.com/profile_images/1615002913545547783/_geo1fcO_400x400.jpg"
-            title="d4te74"
-            subtitle="d4te74@email.com"
-          />
-        </v-list>
-
-        <v-divider />
-
-        <v-list density="compact" nav>
-          <v-list-item
-            v-for="(link, i) in state.links"
-            :key="i"
-            :prepend-icon="link.icon"
-            :title="link.title"
-            :value="link.title"
-            :to="link.path"
-          />
-        </v-list>
-      </v-navigation-drawer>
-
-      <v-main class="main-content">
-        <div class="main-content-container">
-          <slot />
-        </div>
-      </v-main>
-    </v-layout>
-  </v-app>
+  <div>
+    <slot />
+  </div>
 </template>
 
 <style scoped>
-.main-content {
-  background: #f1f3f8;
-}
-
-div.main-content-container {
-  width: 95%;
-  margin: 0 auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-}
 </style>
--- src/client/pages/leagues/[id].vue
+++ src/client/pages/leagues/[id].vue
@@ -14,66 +14,9 @@ const selectedSeasonId = ref<number | undefined>(latestSeason.value?.id);
 
 <template>
   <div v-if="league">
-    <div class="league-wrapper">
-      <v-card outlined elevation="4" class="league-profile" :style="`background-image: url(${league.logo_path})`">
-        <div class="league-profile-content">
-          <img :src="league.logo_path">
-        </div>
-
-        <div class="league-profile-content">
-          <h1>{{ league.name }}</h1>
-        </div>
-      </v-card>
-
-      <div>
-        <StandingsTableList v-if="selectedSeasonId" :season-id="selectedSeasonId" />
-      </div>
-    </div>
+    {{ league }}
   </div>
 </template>
 
 <style lang="scss" scoped>
-div.league-wrapper {
-
-  .league-profile {
-    margin-bottom: 10px;
-    display: flex;
-    background-repeat: no-repeat;
-    background-size: cover;
-    padding: 12px;
-    background-color: #666666;
-    position: relative;
-    z-index: 0;
-    gap: 25px;
-
-    .league-profile-content {
-      align-items: center;
-      display: flex;
-      color: white;
-      text-shadow: 0 0 10px #333;
-
-      img {
-        background: #fff;
-        border-radius: 12px;;
-        width: 150px;
-      }
-
-      h1 {
-        font-size: 44px;
-      }
-
-      &::before {
-        content: '';
-        position: absolute;
-        top: -5px;
-        bottom: -5px;
-        left: -5px;
-        right: -5px;
-        background: inherit;
-        filter: blur(20px);
-        z-index: -1;
-      }
-    }
-  }
-}
 </style>
