fa65e0d0ddde953ee361480ff5b29d6128045229
Hiroaki Date <mmst3l4@gmail.com>
2023-02-19 22:02:38 +0900

    シーズンに紐づくスタッツを取得するAPIを実装

--- src/server/utils/getSeasonStatsById.ts
+++ src/server/utils/getSeasonStatsById.ts
@@ -6,6 +6,8 @@ const { apiPath, apiKey } = useSportmonksApi();
 export const getSeasonStatsById = async (args: { seasonId: number }): Promise<GetStandingsBySeasonIdData> => {
   const { seasonId } = args;
 
-  const response: any = await $fetch(`${apiPath}/seasons/${seasonId}?api_token=${apiKey}&include=stats`);
+  const response: any = await $fetch(
+    `${apiPath}/seasons/${seasonId}?api_token=${apiKey}&include=stats,stats.topscorer,stats.assisttopscorer,stats.mostcleansheetsteam,stats.mostcleansheetsgoalkeeper,stats.mostgoalsteam,stats.mostgoalspermatchteam,stats.mostconcededgoalsteam,stats.mostcornersteam`
+  );
   return response.data;
 };
